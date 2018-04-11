d63859e984b5cce421373f80caf7ff616cd0a305
Josue Ferrufino <otaku14715@gmail.com>
2018-04-10 20:42:05 -0400

    1. creando primer test si cero es primo

---
+++ backend/urbanager-application/src/main/java/com/alenasoft/urbanager/resources/Operations/Prime.java
@@ -0,0 +1,9 @@
+package com.alenasoft.urbanager.resources.Operations;
+
+public class Prime {
+
+    public String holaMundo(){
+        return  "hola";
+    }
+
+}
---
+++ backend/urbanager-application/src/test/java/com/alenasoft/urbanager/resources/example/service/PrimeTest.java
@@ -0,0 +1,36 @@
+package com.alenasoft.urbanager.resources.example.service;
+
+import com.alenasoft.urbanager.api.Example;
+import com.alenasoft.urbanager.resources.Operations.Prime;
+import com.alenasoft.urbanager.resources.example.dao.ExampleDao;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class PrimeTest {
+
+    private final ExampleDao dao = Mockito.mock(ExampleDao.class);
+    private final Example expected = new Example(1, "Test title", "Test Message");
+
+    @Before
+    public void setUp() {
+
+    }
+
+    @After
+    public void tearDown() {
+
+    }
+
+    @Test
+    public void testNumberPrimeIsZero() {
+        Prime prime = new Prime();
+        String result = prime.isPrime(0);
+        String expected = "OK";
+        assertThat(result).isEqualTo(expected);
+    }
+}
